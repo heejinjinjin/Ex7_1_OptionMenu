11547e990d8708e2abe4024d61572dc6034bd85a
heejinjinjin <sshj2917@gmail.com>
2024-05-23 14:15:48 +0900

    직접 풀어보기 7-1 완성

--- app/src/main/java/kr/ac/kopo/ex7_1_optionmenu/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/ex7_1_optionmenu/MainActivity.kt
@@ -6,17 +6,20 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     lateinit var linear : LinearLayout
-    lateinit var btn : Button
-
+    lateinit var imgV : ImageView
+    lateinit var editDegree : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         linear = findViewById<LinearLayout>(R.id.linear)
-        btn = findViewById<Button>(R.id.btn)
+        imgV = findViewById<ImageView>(R.id.imgV)
+        editDegree = findViewById<EditText>(R.id.editDegree)
 
     }
 
@@ -32,25 +35,24 @@ class MainActivity : AppCompatActivity() {
     // 항목이 선택 되었을 때 이벤트 처리할 수 있는 메소드
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.itemRed -> {
-                linear.setBackgroundColor(Color.RED)
-                return true
-            }
-            R.id.itemGreen -> {
-                linear.setBackgroundColor(Color.GREEN)
-                return true
-            }
-            R.id.itemBlue -> {
-                linear.setBackgroundColor(Color.BLUE)
-                return true
-            }
 
-            R.id.btnRotate -> {
-                btn.rotation += 60f
+            R.id.itemRotate -> {
+                imgV.rotation = editDegree.text.toString().toFloat()
                 return true
             }
-            R.id.btnZoomin -> {
-                btn.scaleX = 2f
+            R.id.itemHanra -> {
+                imgV.setImageResource(R.drawable.img1)
+                item.setChecked(true)
+                return true
+            }
+            R.id.itemChuja -> {
+                imgV.setImageResource(R.drawable.img2)
+                item.setChecked(true)
+                return true
+            }
+            R.id.itemBum -> {
+                imgV.setImageResource(R.drawable.img3)
+                item.setChecked(true)
                 return true
             }
         }
